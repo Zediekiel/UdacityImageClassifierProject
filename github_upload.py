fc6195d6378192f743fcd2861310249724d5cf80
Zediekiel <mccandlissian@gmail.com>
2023-10-20 17:02:14 +0000

    Initial Commit

--- github_upload.py
+++ github_upload.py
@@ -3,3 +3,7 @@ git config --global user.email "mccandlissian@gmail.com"
 git config --global user.name "Zediekiel"
 git add -A
 git commit -m "Initial Commit"
+
+git remote add origin https://github.com/Zediekiel/UdacityImageClassifierProject.git
+git branch -M main
+git push -u origin main
